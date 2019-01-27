e444ee9ae936ad3d9d9932197a5d0c04ba4f1c8f
Nicholas Strick <strickolas719@gmail.com>
2019-01-27 12:25:39 -0500

    Script to generate county averages is complete

--- county_profiles.py
+++ county_profiles.py
@@ -16,7 +16,7 @@ def open_csv(csv_loc, contains_excess=True, transpose=False):
 def main():
     guy = dict()
     for ctr in range(10, 17):
-        data = open_csv("data/socioeconomic_" + str(ctr) + ".csv", transpose=False)
+        data = open_csv("data/socioeconomic_" + str(ctr) + ".csv")
         for row in data:
             if row[1] in guy:
                 temp = [row[0]]
@@ -30,12 +30,24 @@ def main():
                 guy[row[1]] = [float(row[0])]
                 guy[row[1]].extend([float(i) for i in row[2:]])
 
+    with open("data/socioeconomic_10.csv") as c_s_v:
+        reader = csv.reader(c_s_v)
+        columns = next(reader)
+    temp = columns[0]
+    columns[0] = columns[1]
+    columns[1] = temp
+    with open("data/socioeconomic_avg.csv", 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerow(columns)
         for k in guy:
             key, val = k, guy[k]
             for x in range(len(val)):
                 val[x] = round(val[x]/7, 2)
             print(key, " -> ", val)
-
+            temp = [key]
+            temp.extend(val)
+            writer.writerow(temp)
+            
 
 if __name__ == "__main__":
     main()
